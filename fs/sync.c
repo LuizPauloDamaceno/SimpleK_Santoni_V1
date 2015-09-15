b343d8f56a5fe0d8ed3a2d626188d020355cce80
andip71 <andreasp@gmx.de>
2015-09-15 16:26:12 +0200

    Dynamic Sync Control 2.0
    While screen is on, file sync is suspended.
    When screen is off, a file sync is called to flush all outstanding writes
    and restore file sync operation as normal.
    
    By andip71 (aka Lord Boeffla)
    
    Credits for original implementation and concept to Faux123
    
    Change-Id: I9e8a4b758b64389e886166c4fe7c2eb9aaf3d545
    
    Conflicts:
    	fs/sync.c
    
    (cherry picked from commit d088f0d540cee29a4fb21e2a1be40ff4fe8b9f79)

--- fs/sync.c
+++ fs/sync.c
@@ -18,8 +18,9 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
-bool fsync_enabled = false;
-module_param(fsync_enabled, bool, 0755);
+#ifdef CONFIG_DYNAMIC_FSYNC
+#include <linux/dyn_sync_cntrl.h>
+#endif
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -92,6 +93,21 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
 {
 	filemap_fdatawait(bdev->bd_inode->i_mapping);
 }
+
+#ifdef CONFIG_DYNAMIC_FSYNC
+/*
+ * Sync all the data for all the filesystems (called by sys_sync() and
+ * emergency sync)
+ */
+void sync_filesystems(int wait)
+{
+	iterate_supers(sync_inodes_one_sb, NULL);
+	iterate_supers(sync_fs_one_sb, &wait);
+	iterate_supers(sync_fs_one_sb, &wait);
+	iterate_bdevs(fdatawrite_one_bdev, NULL);
+	iterate_bdevs(fdatawait_one_bdev, NULL);
+}
+#endif
 
 /*
  * Sync everything. We start by waking flusher threads so that most of
@@ -184,6 +200,16 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
+	struct inode *inode = file->f_mapping->host;
+
+	if (!fsync_enabled)
+		return 0;
+	if (!file->f_op || !file->f_op->fsync)
+
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && suspend_active))
+		return 0;
+#endif
 	if (!file->f_op->fsync)
 	if (!fsync_enabled)
 		return 0;
@@ -233,6 +259,10 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 		return 0;
 
 	return do_fsync(fd, 0);
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && suspend_active))
+		return 0;
+#endif
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
@@ -241,6 +271,10 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 		return 0;
 		
 	return do_fsync(fd, 1);
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && suspend_active))
+		return 0;
+#endif
 }
 
 /*
@@ -299,8 +333,10 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
 
-	if (!fsync_enabled)
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && suspend_active))
 		return 0;
+#endif
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
@@ -382,5 +418,10 @@ out:
 SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
+#ifdef CONFIG_DYNAMIC_FSYNC
+	if (likely(dyn_fsync_active && suspend_active))
+		return 0;
+#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
+
